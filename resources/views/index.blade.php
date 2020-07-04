4704dfddfe3f30086bafc1f0e6e22e05c041cd9d
SalatID <31203124+SalatID@users.noreply.github.com>
2020-07-05 00:37:16 +0700

    erit report

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,6 +24,8 @@
     <script src="/assets/js/vendor/modernizr-2.8.3.min.js"></script>
     <!-- jquery latest version -->
     <script src="/assets/js/vendor/jquery-2.2.4.min.js"></script>
+    <!-- start highcharts js -->
+    <script src="https://code.highcharts.com/highcharts.js"></script>
     <style media="screen">
       .liveSearch{
         position: absolute;
@@ -83,8 +85,7 @@
 
     <!-- start chart js -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
-    <!-- start highcharts js -->
-    <script src="https://code.highcharts.com/highcharts.js"></script>
+
     <!-- start zingchart js -->
     <script src="https://cdn.zingchart.com/zingchart.min.js"></script>
     <script>
--- resources/views/page/dashboard.blade.php
+++ resources/views/page/dashboard.blade.php
@@ -8,55 +8,148 @@
      <!-- sales report area start -->
      <div class="sales-report-area mt-5 mb-5">
          <div class="row">
-             <div class="col-md-4">
+             <div class="col-md-6">
                  <div class="single-report mb-xs-30">
-                     <div class="s-report-inner pr--20 pt--30 mb-3">
-                         <div class="icon"><i class="fa fa-btc"></i></div>
-                         <div class="s-report-title d-flex justify-content-between">
-                             <h4 class="header-title mb-0">Bitcoin</h4>
-                             <p>24 H</p>
-                         </div>
-                         <div class="d-flex justify-content-between pb-2">
-                             <h2>$ 4567809,987</h2>
-                             <span>- 45.87</span>
-                         </div>
+                     <div class="s-report-inne">
+                       <div id="siswa-ekskul" width="100%"></div>
                      </div>
-                     <canvas id="coin_sales1" height="100"></canvas>
+
                  </div>
              </div>
-             <div class="col-md-4">
+             <div class="col-md-6">
                  <div class="single-report mb-xs-30">
-                     <div class="s-report-inner pr--20 pt--30 mb-3">
-                         <div class="icon"><i class="fa fa-btc"></i></div>
-                         <div class="s-report-title d-flex justify-content-between">
-                             <h4 class="header-title mb-0">Bitcoin Dash</h4>
-                             <p>24 H</p>
-                         </div>
-                         <div class="d-flex justify-content-between pb-2">
-                             <h2>$ 4567809,987</h2>
-                             <span>- 45.87</span>
-                         </div>
-                     </div>
-                     <canvas id="coin_sales2" height="100"></canvas>
-                 </div>
-             </div>
-             <div class="col-md-4">
-                 <div class="single-report">
-                     <div class="s-report-inner pr--20 pt--30 mb-3">
-                         <div class="icon"><i class="fa fa-eur"></i></div>
-                         <div class="s-report-title d-flex justify-content-between">
-                             <h4 class="header-title mb-0">Euthorium</h4>
-                             <p>24 H</p>
-                         </div>
-                         <div class="d-flex justify-content-between pb-2">
-                             <h2>$ 4567809,987</h2>
-                             <span>- 45.87</span>
-                         </div>
-                     </div>
-                     <canvas id="coin_sales3" height="100"></canvas>
+                   <div id="siswa-perkelas" width="100%"></div>
                  </div>
              </div>
          </div>
      </div>
  </div>
+ <script type="text/javascript">
+   Highcharts.chart('siswa-ekskul', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Presentasi Siswa yang Mengikuti Ekskul'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    accessibility: {
+        point: {
+            valueSuffix: '%'
+        }
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+            }
+        }
+    },
+    series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+            name: 'Chrome',
+            y: 61.41,
+            sliced: true,
+            selected: true
+        }, {
+            name: 'Internet Explorer',
+            y: 11.84
+        }, {
+            name: 'Firefox',
+            y: 10.85
+        }, {
+            name: 'Edge',
+            y: 4.67
+        }, {
+            name: 'Safari',
+            y: 4.18
+        }, {
+            name: 'Sogou Explorer',
+            y: 1.64
+        }, {
+            name: 'Opera',
+            y: 1.6
+        }, {
+            name: 'QQ',
+            y: 1.2
+        }, {
+            name: 'Other',
+            y: 2.61
+        }]
+    }]
+  });
+  Highcharts.chart('siswa-perkelas', {
+   chart: {
+       plotBackgroundColor: null,
+       plotBorderWidth: null,
+       plotShadow: false,
+       type: 'column'
+   },
+   title: {
+       text: 'Siswa Mengikuti Ekskul Per Kelas'
+   },
+   tooltip: {
+       pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+   },
+   accessibility: {
+       point: {
+           valueSuffix: '%'
+       }
+   },
+   plotOptions: {
+       pie: {
+           allowPointSelect: true,
+           cursor: 'pointer',
+           dataLabels: {
+               enabled: true,
+               format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+           }
+       }
+   },
+   series: [{
+       name: 'Brands',
+       colorByPoint: true,
+       data: [{
+           name: 'Chrome',
+           y: 61.41,
+           sliced: true,
+           selected: true
+       }, {
+           name: 'Internet Explorer',
+           y: 11.84
+       }, {
+           name: 'Firefox',
+           y: 10.85
+       }, {
+           name: 'Edge',
+           y: 4.67
+       }, {
+           name: 'Safari',
+           y: 4.18
+       }, {
+           name: 'Sogou Explorer',
+           y: 1.64
+       }, {
+           name: 'Opera',
+           y: 1.6
+       }, {
+           name: 'QQ',
+           y: 1.2
+       }, {
+           name: 'Other',
+           y: 2.61
+       }]
+   }]
+ });
+ </script>
 @endsection
