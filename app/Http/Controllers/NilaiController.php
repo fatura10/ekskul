1c9e583cc3148a21c4e430f2d260babb680d0db2
SalatID <31203124+SalatID@users.noreply.github.com>
2020-07-19 16:19:01 +0700

    Nilai

--- app/Http/Controllers/NilaiController.php
+++ app/Http/Controllers/NilaiController.php
@@ -9,6 +9,21 @@ class NilaiController extends Controller
 {
     public function saveNilai(Request $req)
     {
-      dd($req->all());
+      $insertVal=0;
+      for ($i=0; $i < count($req->nilai); $i++) {
+        $dataInsert = [
+          "id_siswa"=>$req->id_siswa[$i],
+          "id_nilai"=>($req->nilai[$i]==null?0:$req->nilai[$i]),
+          "id_jadwa"=>$req->id_jadwal
+        ];
+        print_r($dataInsert);
+        if (!Nilai::insertData($dataInsert)['error']) {
+          $insertVal = $insertVal+1;
+        }
+      }
+      if ($insertVal==count($req->nilai)) {
+        //return redirect()->back()->with(["error"=>false,"message"=>"Tambah Nilai Berhasil"]);
+      }
+      //return redirect()->back()->with(["error"=>"001","message"=>"Tambah Nilai Gagal"]);
     }
 }
--- app/Nilai.php
+++ app/Nilai.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Validator;
+use Session;
 
 class Nilai extends Model
 {
@@ -15,5 +17,34 @@ class Nilai extends Model
         return generateId();
     }
     return $number;
+  }
+  public static function validateForm($data)
+  {
+    $formValidate = [
+        'nilai' => 'required',
+        'id_siswa' => 'required',
+        'id_jadwal' => 'required'
+    ];
+    $validator = Validator::make($data,$formValidate );
+
+    if ($validator->fails()) {
+      return false;
+    }
+    return true;
+  }
+
+  public static function insertData($data)
+  {
+    if (self::validateForm($data)) {
+      $data["id"]=self::generateId();
+      $data["created_dt"]=date("Y-m-d H:i:s");
+      $data["created_user"]=SESSION::get('userData')['userData']['user_id'];
+      //dd(self::insert($data));
+      if (self::insert($data)) {
+        return ["error"=>false,"message"=>"Tambah Siswa Berhasil"];
+      }
+      return ["error"=>"001","message"=>"Tambah Siswa Gagal"];
+    }
+    return ["error"=>"001","message"=>"Field ada yang kosong"];
   }
 }
